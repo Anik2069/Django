f638f5116ccf2d246c8f5ab1c18e1f9cc3f69564
Md Anik Islam <anikislam668@gmail.com>
2020-08-12 22:47:45 +0600

    Food Donor Profile

--- food_donation/food_doner/models.py
+++ food_donation/food_doner/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from  django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
 class Doner_Post(models.Model):
     date = models.DateField()
@@ -16,9 +17,16 @@ class Doner_Post(models.Model):
 class profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     image = models.ImageField(default= 'default.jpg', upload_to = 'profile_pics')
+    number = models.CharField(max_length = 100)
+    address= models.CharField(max_length = 100)
 
     def __str__(self):
         return  f'{self.user.username} Profile'
+
+    def save(self):
+        super.save()
+
+        img = Image.open(self.image.path)
 
 
 
