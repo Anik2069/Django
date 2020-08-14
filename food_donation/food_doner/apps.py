e15e6388baf1d16f6214f6aca841d8c29cef7dc3
Md Anik Islam <anikislam668@gmail.com>
2020-08-14 13:24:18 +0600

    Bug -01 : Profile Not Found .. # SOlved

--- food_donation/food_doner/apps.py
+++ food_donation/food_doner/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class FoodDonerConfig(AppConfig):
     name = 'food_doner'
+
+    def ready(self):
+        import food_doner.signals
--- food_donation/food_doner/models.py
+++ food_donation/food_doner/models.py
@@ -28,8 +28,8 @@ class profile(models.Model):
     def __str__(self):
         return  f'{self.user.username} Profile'
 
-    # def save(self):
-    #     super.save()
+    #def save(self):
+    #    super.save()
     #     img = Image.open(self.image.path)
     #     if img.height > 300 or img.width > 300:
     #         output_size= (300,300)
